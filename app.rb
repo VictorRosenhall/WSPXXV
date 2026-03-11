46a2f248c4355f6df5c753c6ec2d18adc70fde91
VictorRosenhall <victor.rosenhall@gmail.com>
2026-03-11 16:27:51 +0100

    Oj vad jag glömt commita

--- app.rb
+++ app.rb
@@ -3,3 +3,140 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+enable :session
+
+DB_PATH = "db/databas.db"
+
+def db
+  return @db if @db
+
+  @db = SQLite3::Database.new(DB_PATH)
+  @db.results_as_hash = true
+  @db
+end
+
+get('/') do
+  slim(:index)
+end
+
+#get ('/purchase') do
+#  query = params[:q]
+#
+#  if query && !query.strip.empty?
+#    @purchase = db.execute(
+#      "SELECT * FROM PURCHASE WHERE name LIKE ?",
+#      ["%#{query}%"]
+#    )
+#  else
+#    @purchase = db.execute("SELECT * FROM PURCHASE")
+#  end
+#
+#  slim(:purchase)
+#end
+
+get('/purchase') do
+  query = params[:q]
+
+  if query && !query.strip.empty?
+    @purchase = db.execute(
+      "SELECT * FROM purchase WHERE name LIKE ?",
+      ["%#{query}%"]
+    )
+  else
+    @purchase = db.execute("SELECT * FROM purchase")
+  end
+
+  slim(:purchase)
+end
+
+post('/purchase') do
+  name = params[:q]
+  description = params[:description]
+
+  db.execute("INSERT INTO purchase (name) VALUES (?)", [name])
+
+  redirect('/purchase')
+end
+
+#post('/todos/:id/delete') do
+#  db = SQLite3::Database.new('db/todos.db') # koppling till databasen
+#  #extrahera id för att få rätt frukt
+#  denna_ska_bort = params[:id]
+#  #ta bort från db
+#  db.execute("DELETE FROM todos WHERE id = ?", [denna_ska_bort])
+#  redirect('/todos')
+#end
+#
+#get('/todos/:id/edit') do
+#  db = SQLite3::Database.new("db/todos.db")
+#  db.results_as_hash = true
+#
+#  id = params[:id].to_i
+#  @todo = db.execute("SELECT * FROM todos WHERE id = ?", [id]).first
+#
+#  slim(:edit)
+#end
+#
+#post('/todos/:id/update') do
+#  db = SQLite3::Database.new("db/todos.db")
+#
+#  id = params[:id].to_i
+#  name = params[:name]
+#  description = params[:description]
+#
+#  db.execute("UPDATE todos SET name=?, description=? WHERE id=?", [name, description, id])
+#
+#  redirect('/todos')
+#end
+#
+#post('/todos') do
+#  db = SQLite3::Database.new("db/todos.db")
+#  name = params[:q]
+#  description = params[:description]
+#  db.execute("INSERT INTO todos (name, description) VALUES (?, ?)", [name, description])
+#  redirect('/todos')
+#end
+#
+#get('/user') do
+#  user = params["user"]
+#  pwd = params["pwd"]
+#  pwd_confirm = params["pwd_confirm"]
+#
+#  db = SQLite3::Database.new("db/store.db")
+#  result=db.execute("SELECT id FROM users WHERE user=?",user)
+#
+#  if result.empty?
+#    if pwd==pwd_confirm
+#      pwd_digest=BCrypt::Password.create(pwd)
+#      db.execute("INSERT INTO users(user,pwd_digest) VALUES(?,?)", [user,pwd_digest])
+#      redirect('/welcome')
+#    else
+#      redirect('/error') #om lösenord it matchar
+#    end
+#  else
+#    redirect('/login') #om d redan finns
+#  end
+#  slim(:register)
+#end
+#
+#get('/login') do
+#    user = params["user"]
+#    pwd = params["pwd"]
+#
+#    db = SQLite3::Database.new("db/store.db")
+#    result=db.execute("SELECT id,pwd_digest FROM users WHERE user=?",user)
+#
+#    if result.empty?
+#      redirect('/error')
+#    end
+#    
+#    user_id = result.first["id"]
+#    pwd_digest = result.first["pwd_digest"]
+#
+#    if BCrypt::Password.new(pwd_digest) == pwd
+#      session[:user_id] = user_id
+#      redirect('/welcome')
+#    else
+#      redirect('/error')
+#    end
+#  end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,10 +1,9 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new("databas.db")
-
+db = SQLite3::Database.new("db/databas.db")
 
 def seed!(db)
-  puts "Using db file: db/todos.db"
+  puts "Using db file: db/databas.db" 
   puts "🧹 Dropping old tables..."
   drop_tables(db)
   puts "🧱 Creating tables..."
@@ -15,21 +14,39 @@ def seed!(db)
 end
 
 def drop_tables(db)
-  db.execute('DROP TABLE IF EXISTS exempel')
+  db.execute('DROP TABLE IF EXISTS USER_PURCHASE_REL')
+  db.execute('DROP TABLE IF EXISTS USERS')
+  db.execute('DROP TABLE IF EXISTS purchase')
+  db.execute('DROP TABLE IF EXISTS CATEGORY')
 end
 
 def create_tables(db)
-  db.execute('CREATE TABLE exempel (
+  db.execute('CREATE TABLE USERS (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               name TEXT NOT NULL, 
-              description TEXT,
-              state BOOLEAN)')
+              pwd_digest TEXT)')
+  db.execute('CREATE TABLE purchase (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL, 
+              type_id TEXT)')
+  db.execute('CREATE TABLE CATEGORY (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL)')
+  db.execute('CREATE TABLE USER_PURCHASE_REL (
+              p_id INTEGER,
+              u_id INTEGER,
+              PRIMARY KEY (p_id, u_id),
+              FOREIGN KEY (p_id) REFERENCES purchase(id)
+                ON DELETE CASCADE,
+              FOREIGN KEY (u_id) REFERENCES USERS(id)
+                ON DELETE CASCADE
+              )')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp mjölk", "3 liter mellanmjölk, eko",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp julgran", "En rödgran",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten",false)')
+  db.execute('INSERT INTO USERS (name, pwd_digest) VALUES ("Elias", "Benis")')
+  db.execute('INSERT INTO purchase (name, type_id) VALUES ("Cheese burgir", "3")')  
+  db.execute('INSERT INTO CATEGORY (name) VALUES ("FOOD")')
 end
 
 
